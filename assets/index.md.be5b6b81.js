cb619e4a4925ddb162bfaf1cf0286ab13e098aa3
koudui <koudui@users.noreply.github.com>
2022-10-08 08:40:26 +0000

    deploy: 383593d428666a817d0d3e703fcd9445874d7104

---
+++ assets/index.md.be5b6b81.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a as n}from"./app.40d0cb7d.js";const u=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md"}'),o={name:"index.md"},s=n("p",null,"Hello One Space!",-1),c=[s];function r(d,_,i,p,l,m){return a(),t("div",null,c)}const x=e(o,[["render",r]]);export{u as __pageData,x as default};
---
+++ assets/mailpost_index.md.fbd851e7.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.40d0cb7d.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"mailpost/index.md"}'),o={name:"mailpost/index.md"};function n(r,s,c,i,p,d){return a(),t("div")}const l=e(o,[["render",n]]);export{m as __pageData,l as default};
